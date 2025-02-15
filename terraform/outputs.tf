8536276c5c50f8ac93e1aa18f1ec5fa66d06a0e7
pramod999 <pramodmangunta9@gmail.com>
2025-02-15 17:42:06 +0530

    ds

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -15,6 +15,6 @@ output "region" {
 
 
 output "cluster_security_group_id" {
-  description = "Security group ID for the Amazon Web Service EKS Cluster "
+  description = "Security groups ID for the Amazon Web Service EKS Cluster "
   value       = module.eks.cluster_security_group_id
 }
